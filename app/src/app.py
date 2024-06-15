7ee5b7782df115143a92183940cb043b61aa1784
Sabir MOHAMED BEN ALI <sabir.mohamedbenali@metanext.com>
2024-06-15 13:24:08 +0000

    chat handled

--- app/src/app.py
+++ app/src/app.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import subprocess
-import os
-from flask_cors import CORS
-from datetime import timedelta
-
-PATH_KEY = ".secret.key"
-HOST = os.getenv("HOST", "localhost")
-
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
-CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///locations.db'
+db = SQLAlchemy(app)
+geolocator = Nominatim(user_agent="myapp")
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Location {self.id} ({self.latitude}, {self.longitude})>'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/locate', methods=['POST'])
+def locate():
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    location = Location(latitude=latitude, longitude=longitude)
+    db.session.add(location)
+    db.session.commit()
+    return jsonify({'status': 'success'})
+
+@app.route('/follow')
+def follow():
+    locations = Location.query.order_by(Location.id.desc()).limit(10).all()
+    return jsonify([{'latitude': location.latitude, 'longitude': location.longitude} for location in locations])
+
+if __name__ == '__main__':
+    #db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
